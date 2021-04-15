85c11d909ebcb37ef9621ae80d668da9476110a2
Luomo <guoruiqiang_2018@163.com>
2021-04-15 23:18:23 +0800

    初始化SmartHttp项目

---
+++ src/main/java/fun/luomo/smart/Test.java
@@ -0,0 +1,11 @@
+package fun.luomo.smart;
+
+/**
+ * @author luomo
+ * @date  2021/4/15 23:09
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello SmartHttp");
+    }
+}
